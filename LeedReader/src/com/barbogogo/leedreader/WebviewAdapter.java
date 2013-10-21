946a67631bb5512f29726a33d9bfad93a851d4c7
Barbogogo <barbogogo@barbogogo.fr>
2013-10-21 18:53:08 +0200

    Correction : Problème de lien Correction suite au ticket #65

--- LeedReader/src/com/barbogogo/leedreader/WebviewAdapter.java
+++ LeedReader/src/com/barbogogo/leedreader/WebviewAdapter.java
@@ -117,8 +117,14 @@ public class WebviewAdapter extends PagerAdapter
 
         while (matches2.find())
         {
-            String replace = matches2.group();
-            replace += "<a onClick=\"showAndroidToast('" + matches2.group(2) + "')\">";
+            String link = "";
+            if(matches2.group(2).startsWith("//"))
+                link = "http:" + matches2.group(2);
+            else
+                link = matches2.group(2);
+            
+            String replace = "<iframe " + matches2.group(1)+"src=\""+link+"\""+ matches2.group(3)+"></iframe>";
+            replace += "<a onClick=\"showAndroidToast('" + link + "')\">";
             replace += "Ouvrir en externe</a>";
 
             found.add(replace);
